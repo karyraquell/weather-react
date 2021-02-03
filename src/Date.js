952268ca782a0bf264191a7dc4af64c0d7c675b9
Karina <karyraquell@outlook.com>
2021-02-03 11:01:31 -0500

    Create Date.js

---
+++ src/Date.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+export default function Date() {
+    return (
+        <div className="Date">
+            <h1 className="City">Parma</h1>
+            <h2 className="Dates">Friday, January 15, 2021</h2>
+            <h3 className="Time">10:14 A.M</h3>
+        </div>
+    )
+}
